0222705d5ba5472d39ce6a18337f68018b2b81bf
Niko123454321 <90371160+Niko123454321@users.noreply.github.com>
2022-11-20 21:58:19 +0000

    Deploying to gh-pages from @ Niko123454321/goit-js-hw-08@9685a9e945481f805d82d06870f04ce6c162698e 🚀

---
+++ 02-video.ac61eaee.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("#vimeo-player");console.log(e)}();
+//# sourceMappingURL=02-video.ac61eaee.js.map
---
+++ 02-video.bdc31991.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("#vimeo-player");console.log(e);
+//# sourceMappingURL=02-video.bdc31991.js.map
